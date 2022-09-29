8103ae72190ed716c236705348005ac76b22dd85
Jacob Mastel <jacob.mastel@oregonstate.edu>
2022-09-29 11:23:08 -0700

    started new example

---
+++ python-publish-csv-to-tableau/main.py
@@ -0,0 +1,17 @@
+import tableauserverclient as TSC
+import os
+import sys
+
+from dotenv import load_dotenv
+from typing import List
+
+
+load_dotenv()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
